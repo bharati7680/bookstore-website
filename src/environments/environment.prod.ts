c1d32f3ac50820d919ba6f49f983d2c1ea9209c2
Bharati Kudambal <bharatikudambal@gmail.com>
2023-03-23 00:58:07 +0530

    environment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  API_URL: "https://bookstore-api-850c.up.railway.app/",
+  CALLBACK_URL: "http://localhost:4200/my-orders"
+}
